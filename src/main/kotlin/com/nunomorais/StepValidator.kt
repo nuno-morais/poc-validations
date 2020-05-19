bbbce6341ff794e83244e4be27c02079601555b0
Nuno Morais <nuno.morais@talkdesk.com>
2020-05-19 15:45:49 +0100

    Support MinLength

--- src/main/kotlin/com/nunomorais/StepValidator.kt
+++ src/main/kotlin/com/nunomorais/StepValidator.kt
@@ -1,5 +1,6 @@
 package com.nunomorais
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
 import kotlin.reflect.KType
@@ -9,9 +10,12 @@ import kotlin.reflect.full.isSubclassOf
 import kotlin.reflect.full.memberProperties
 import kotlin.reflect.jvm.jvmErasure
 
-annotation class FieldValidation(
-    val type: KClass<out Validator>
+annotation class MinLengthFieldValidation(
+    val type: KClass<out Validator>,
+    val min: Int
 )
+
+annotation class FieldValidation
 
 data class ResultValidator(
     val isValid: Boolean = true,
@@ -62,24 +66,73 @@ class StepValidatorProcessor {
                 matchingType.validate(target, type.returnType).let {
                     if (!it.isValid) {
                         mapOf(path to listOf(it.error))
-                    } else mapOf()
+                    } else {
+                        applyFieldValidations(type, target, path)
+                    }
                 }
             }
         }
 
     private fun applyFieldValidations(
         type: KProperty<*>,
-        target: Map<*, *>,
+        target: Any,
         path: String
-    ) = type.annotations { annotationName -> annotationName.endsWith("FieldValidation") }
-        .fold(mapOf<String, Any>()) { acc, it ->
-            it::class.memberProperties.find { prop -> prop.name == "type" }?.let { prop ->
-                val validatorType = prop.getter.call(it) as KClass<out Validator>
-                val customValidator = validatorType.createInstance()
-                val result = customValidator.validate(target, type.returnType)
+    ) = type.annotations { annotationName -> annotationName.endsWith(FieldValidation::class.simpleName!!) }
+        .fold(mapOf<String, Any>()) { acc, annotation ->
+            annotation::class.memberProperties.find { prop -> prop.name == "type" }?.let { prop ->
+                val validatorType = annotation.javaClass.getMethod(prop.name).invoke(annotation) as Class<out Validator>
+                val customValidator = validatorType.newInstance()
+                val args = annotation::class.memberProperties
+                    .filter { p -> p.name != prop.name && p.name != "h" }
+                    .map { argument ->
+                        annotation.javaClass.getMethod(argument.name).invoke(annotation)
+                    }.toTypedArray()
+                val result = customValidator.validate(target, type.returnType, *args)
                 acc + if (!result.isValid) {
                     mapOf(path to listOf(result.error))
                 } else emptyMap()
             } ?: emptyMap()
         }
+}
+
+data class FooBar(
+    val list: List<Int>
+)
+
+data class Bar(
+    val name: String,
+    val foo: FooBar
+)
+
+data class Foo(
+    @field:MinLengthFieldValidation(type = MinLengthValidator::class, min = 5)
+    val age: Int?,
+    val bar: Bar,
+    val bool: Boolean
+)
+
+
+class MinLengthValidator : Validator {
+    override fun validate(target: Any, type: KType, vararg args: Any) =
+        if (target is Number && args.first() is Number) {
+            if (target.toDouble() < (args.first()!! as Number).toDouble()) {
+                ResultValidator(false, "MIN_LENGTH")
+            } else ResultValidator()
+        } else {
+            ResultValidator(false, "UNHANDLED_EXCEPTION")
+        }
+}
+
+fun main() {
+    val m = mapOf(
+        "age" to 6,
+        "bar" to mapOf(
+            "name" to "hello1",
+            "foo" to mapOf(
+                "list" to listOf(1, 2, 3)
+            )
+        ),
+        "bool" to false
+    )
+    println(StepValidatorProcessor().validate("", m, Foo::class))
 }
